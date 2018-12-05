f36a7481706d1560dd8c5bf7c93500b98d735edc
Akash Gupta <akashg524@gmail.com>
2018-12-05 18:15:40 +0530

    corrected spelling mistakes

--- Source/Services/QueueBuilder.cs
+++ Source/Services/QueueBuilder.cs
@@ -33,6 +33,7 @@ namespace KnowledgeGraph.Services {
             Model.QueueDeclare ("QuizEngine_KnowledgeGraph_QuestionBatch", false, false, false, null);
             Model.QueueDeclare ("Profile_KnowledgeGraph_LearningPlanFeedBack", false, false, false, null);
             Model.QueueDeclare ("Profile_KnowledgeGraph_ResourceFeedBack", false, false, false, null);
+            Model.QueueDeclare ("Profile_KnowledgeGraph_QuestionFeedBack", false, false, false, null);
 
             Model.QueueBind ("KnowledgeGraph_Contributer_Ids", ExchangeName, "Request.Question");
             Model.QueueBind ("Contributer_KnowledgeGraph_LearningPlan", ExchangeName, "Models.LearningPlan");
